964e87e6e4f3fd5c8203dd2943ad81b188bf031f
unknown <annaldasvachaspati@gmail.com>
2025-02-02 20:12:56 +0530

    Frontend Updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,12 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <Link className="navbar-brand" to="/">React Tasks</Link>
+        <Link className="navbar-brand" to="/">Frontend Tasks</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+          <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <Link className="nav-link" to="/counter">Counter</Link>
             </li>
